eff08293f6e38a3af0cd414a6d9432f317866ab0
lubahabarta <lubahabarta@gmail.com>
2022-10-07 11:58:18 +0200

    onSnapshot funkce - pokazde kdyz se v databazi neco zmeni, fetchne data

--- src/App.js
+++ src/App.js
@@ -4,29 +4,44 @@ import { initializeApp } from 'firebase/app'
 import { 
 	getFirestore,
 	collection, 
-	getDocs } from 'firebase/firestore'
+	getDocs,
+	doc,
+	onSnapshot,
+} from 'firebase/firestore'
 
 const App = () => {
 
 	const [blogs, setBlogs] = useState([]);
 
+
 	initializeApp(firebaseConfig);
 	const db = getFirestore();
-	const colBlogs = collection(db, 'blogs');
 
 	useEffect(() => {
-		getDocs(colBlogs)
-		.then((snapshot) => {
+
+		// (async () => {
+		// 	const querySnapshot = await getDocs(collection(db, 'blogs'));
+		// 	const blogArr = [];
+	
+		// 	querySnapshot.forEach(doc => {
+		// 		const blog = { ...doc.data(), id: doc.id };
+		// 		blogArr.push(blog);
+		// 	});
+	
+		// 	setBlogs(blogArr);
+		// })();
+
+		onSnapshot(collection(db, 'blogs'), querySnapshot => {
 			const blogArr = [];
-			snapshot.docs.forEach(doc => {
+	
+			querySnapshot.forEach(doc => {
 				const blog = { ...doc.data(), id: doc.id };
 				blogArr.push(blog);
 			});
+
 			setBlogs(blogArr);
-		})
-		.catch(error => error(error.message));
+		});
 	}, []);
-	
 
 	return (
 		<div className="app">
