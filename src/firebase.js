0343b17247f1a9cad2ded03bcdb1e6361e7b3d44
lubahabarta <lubahabarta@gmail.com>
2022-10-24 19:37:39 +0200

    new apikey

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDrUMwz6lba1cgyk75Yrehe3b6gZYQXPLk",
-    authDomain: "project-2700340055348629958.firebaseapp.com",
-    projectId: "project-2700340055348629958",
-    storageBucket: "project-2700340055348629958.appspot.com",
-    messagingSenderId: "215961202337",
-    appId: "1:215961202337:web:f0fd858f9cc9d59c840ae0",
-    measurementId: "G-ZCTX91DBRL"
+    apiKey: "AIzaSyBpARmbRJypfR1DuZiBZH1cc1ntbdVD82Y",
+    authDomain: "fir-4be22.firebaseapp.com",
+    projectId: "fir-4be22",
+    storageBucket: "fir-4be22.appspot.com",
+    messagingSenderId: "761773519432",
+    appId: "1:761773519432:web:573151ba652f15078b5e7d",
+    measurementId: "G-Y5TD3R5G78"
 }
